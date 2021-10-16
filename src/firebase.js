fce76243cd9e22580b4730de82535598b63a9c95
DESKTOP-BTQFQHB\samsung <ldk4651@gmail.com>
2021-10-16 15:47:52 +0900

    1.0 React + Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAW-3LltbFyI_GbGz7c10ZNV6ijCcvUpvo",
+  authDomain: "nwitter-c074a.firebaseapp.com",
+  projectId: "nwitter-c074a",
+  storageBucket: "nwitter-c074a.appspot.com",
+  messagingSenderId: "224229104109",
+  appId: "1:224229104109:web:24ced128ee98bff3d12fe4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
