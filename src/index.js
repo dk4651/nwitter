0b63fbbd633d661bea068889c7ef18b0427288ec
DESKTOP-BTQFQHB\samsung <ldk4651@gmail.com>
2022-01-26 22:11:07 +0900

    2.1Login form part One & 2.2 Recap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import fbase from 'fbase';
+import App from 'components/App';
+
 
 ReactDOM.render(
   <React.StrictMode>
